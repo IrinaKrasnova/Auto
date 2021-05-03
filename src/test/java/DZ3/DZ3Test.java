a6d44095b73fee9c7afbadd19da8cc1ab4780b56
Irina Krasnova <arisha.17@mail.ru>
2021-05-03 23:54:53 +0300

    DZ3

--- src/test/java/DZ3/DZ3Test.java
+++ src/test/java/DZ3/DZ3Test.java
@@ -9,32 +9,30 @@ import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class DZ3Test {
     private static WebDriver driver;
     private static final String LOGIN_PAGE_URL = "https://www.nn.ru/community/pokupka/glavpristroi/";
     public static void main(String[] args) throws InterruptedException {
-//        WebDriverManager.chromedriver().setup();
-//
-//        driver = new ChromeDriver();
-        System.setProperty("webdriver.chrome.driver", "src/main/resourses/chromedriver_win32/chromedriver.exe");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--disable-popup-blocking");
+        WebDriverManager.chromedriver().setup();
 
-        WebDriver driver = new ChromeDriver(options);
+        driver = new ChromeDriver();
 
         driver.manage().timeouts().implicitlyWait(3, TimeUnit.SECONDS);
 
         login();
 
         driver.findElement(By.xpath("//span[text()='Новая тема']")).click();
+        List<String> windows = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(windows.get(1));
         driver.findElement(By.xpath("//input[@name='Theme']")).sendKeys("Новая тема");
         driver.findElement(By.xpath("//textarea")).sendKeys("Новая тема");
         Select expenceSelect = new Select(driver.findElement(By.xpath("//select[@name='Filter']")));
         expenceSelect.selectByVisibleText("Прочее");
         driver.findElement(By.xpath("//input[@type='submit']")).click();
-        driver.findElement(By.xpath("//input[@type='submit']"));
         driver.findElement(By.xpath("//*[text()='Новая тема успешно добавлена на форум']"));
 
 
